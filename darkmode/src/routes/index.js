146a9df1efa7b78f9c3283186770c72707f23fda
lee <leehao921@gmail.com>
2023-01-04 16:34:37 +0800

    done

---
+++ darkmode/src/routes/index.js
@@ -0,0 +1,16 @@
+import App from './App';
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+// Use something like react-router-dom to manage multiple pages/routes
+
+class Routes extends React.Component {
+
+    
+  render(){
+   return(<App/>)
+  }
+};
+
+// export default Home;
+export default Routes;
